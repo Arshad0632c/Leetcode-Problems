1ab75e5a1a727041e0af2449de8de57dede414e9
Arshad <arshadarshad0632c@gmail.com>
2026-08-06 20:13:20 +0530

    Time: 1 ms (100%), Space: 42.8 MB (77.35%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.java
@@ -2,18 +2,18 @@ class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
         int n=numOnes+numZeros+numNegOnes;
         int sum=0;
-        ArrayList<Integer> arr=new ArrayList<>();
+        int arr[]=new int[n];
         for(int i=0;i<numOnes;i++){
-            arr.add(1);
+            arr[i]=1;
         }
         for(int i=numOnes;i<numOnes+numZeros;i++){
-            arr.add(0);
+            arr[i]=0;
         }
         for(int i=numOnes+numZeros;i<n;i++){
-            arr.add(-1);
+            arr[i]=-1;
         }
         for(int i=0;i<k;i++){
-            sum+=arr.get(i);
+            sum+=arr[i];
         }
         return sum;
     }
